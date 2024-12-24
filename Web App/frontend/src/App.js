26522a280daad7203023c976eda3bfcaaf7b2f56
jyotiranjan862 <dhaljyotiranjan995@gmail.com>
2024-12-24 17:08:45 +0530

    setup

--- Web App/frontend/src/App.js
+++ Web App/frontend/src/App.js
@@ -8,25 +8,32 @@ import Events from "./pages/Events";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Profile from "./pages/Profile";
-
-
-
-
-
+import Dashboard from "./pages/Dashboard";
+import Statistics from "./components/Statistics";
+import Projects from "./components/Projects";
+import Funding from "./components/Funding";
+import Reports from "./components/Reports";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <div className="mt-6 px-6">
+      <div className="min-h-screen">
         <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
           <Route path="/" element={<Homepage />} />
           <Route path="/project-explorer" element={<ProjectExplorer />} />
           <Route path="/knowledge-hub" element={<KnowledgeHub />} />
           <Route path="/events" element={<Events />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="statistics" element={<Statistics />} />
+            <Route path="projects" element={<Projects />} />
+            <Route path="funding" element={<Funding />} />
+            <Route path="reports" element={<Reports />} />
+            <Route path="events" element={<Events />} />
+          </Route>
         </Routes>
       </div>
     </Router>
---
+++ Web App/frontend/src/components/Statistics.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Statistics = () => {
+  return (
+    <div className='bg-red-400 h-[100vh] w-full'>Statistics</div>
+  )
+}
+
+export default Statistics
