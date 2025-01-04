c863af3f4a4d0b85f78a23b344ca6cfe6416c53b
Shibam Mohanty <106046613+SHIBAMMOHANTY@users.noreply.github.com>
2025-01-04 15:20:43 +0530

    vv

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+pp.use(cors({
+    origin: '*', // Accept requests from all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific HTTP methods
+   
+}));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
